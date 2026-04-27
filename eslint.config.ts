a601c29f5e8cf4dc481022a5cbd15a10b2d501c8
Andrew Kazakov <andreww_dev@proton.me>
2026-04-27 10:38:11 +0300

    fix(eslint): globally ignore CHANGELOG.md

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,11 +2,11 @@ import {eslintConfig} from 'eslint-config-un';
 
 export default eslintConfig({
   defaultConfigsStatus: 'misc-enabled',
+  ignores: ['CHANGELOG.md'],
   configs: {
     markdown: {
       configSentencesPerLine: {
         ignores: [
-          'CHANGELOG.md',
           // Putting every sentence on its own line causes line wraps in the changelog
           '.changeset/*.md',
         ],
